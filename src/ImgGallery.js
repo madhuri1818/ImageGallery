c63ada512067c8465b48ef82bb14a3b466331a51
Vellanki Madhuri <116187301+madhuri1818@users.noreply.github.com>
2024-02-01 22:01:40 +0530

    Update ImgGallery.js

--- src/ImgGallery.js
+++ src/ImgGallery.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
 export default function ImgGallery({ image }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const tags = image.tags.split(',');
+
+ console.log({image})
   return (
-    <>
-      <div className="gallery">
-        <a  rel="noreferrer" target="_blank" href={image.largeImageURL}>
-          <div className="card">
-            <img src={image.webformatURL} className="card-img-top" alt="..." />
+    <div className="gallery">
+      <div className="card" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <img src={image.webformatURL} className="card-img-top" alt="..." />
+        
+        {isHovered && (
+          <div className="des">
+          <a rel="noreferrer" target="_blank" href={image.largeImageURL} className="view-link" >
+            View
+          </a>
+          <h3>{tags[0]}</h3>
           </div>
-        </a>
-      </div>
-    </>
+        )}
+        </div>
+      
+    </div>
   );
 }
